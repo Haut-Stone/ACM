8dd3b6ef3cb3159cf23262da5d038f77f0a9079d
Haut-Stone <haut.1604.stone@gmail.com>
2016-12-14 20:55:42 +0800

    饿，边乘边除要记好
    就酱

---
+++ n的阶乘mod P.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    long long n;
+    long long p;
+    long long ans = 1;
+    scanf("%lld %lld", &n, &p);
+    for(int i=1;i<=n;i++){
+        ans = ans*i;
+        ans = ans%p;
+    }
+    printf("%lld\n", ans);
+    return 0;
+}
