b28cef74f9976b4d9f0e3d1dcabeeb9369d9e4ba
Haut-Stone <haut.1604.stone@gmail.com>
2016-12-28 23:14:32 +0800

    暂时po上来
    po

---
+++ 轻院新生赛--以为是贪心。。.cpp
@@ -0,0 +1,24 @@
+/*
+* Created by ShiJiahuan(li) in haut.
+* for more please visit www.shallweitalk.com
+* 
+* Copyright 2016 SJH. All rights reserved.
+*
+* @Author: Haut-Stone
+* @Date:   2016-12-26 12:52:30
+* @Last Modified by:   Haut-Stone
+* @Last Modified time: 2016-12-26 12:52:30
+*/
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+using namespace std;
+
+
+int main(void)
+{
+
+    return 0;
+}
