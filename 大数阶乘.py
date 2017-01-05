48599cfbc11ce8cb7cbbe1d31e0fcef5cbe3dc44
Haut-Stone <haut.1604.stone@gmail.com>
2017-01-05 15:44:15 +0800

    python对大数比C友好多了
    人生苦短，我用python

---
+++ 大数阶乘.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: Haut-Stone
+# @Date:   2017-01-05 15:38:30
+# @Last Modified by:   Haut-Stone
+# @Last Modified time: 2017-01-05 15:43:27
+# 利用了python的科学计算库，和对大数友好的性质
+import math
+N = int(input())
+print(math.factorial(N)) 
