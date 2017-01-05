3b11dfe1c33468dd373d97f5a5e20f8438079c3e
Haut-Stone <haut.1604.stone@gmail.com>
2017-01-05 15:55:29 +0800

    python大法好
    太厉害了FFT

---
+++ 大数乘法.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: Haut-Stone
+# @Date:   2017-01-05 15:50:48
+# @Last Modified by:   Haut-Stone
+# @Last Modified time: 2017-01-05 15:53:58
+# 同样体现了python对大数的友好性
+# 顺带一提，从这一题第一次见识到了快速傅立叶变换。。。的真面目
+import math
+a = int(input())
+b = int(input())
+print(a*b)
