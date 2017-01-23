6c0e113344cd18f6116d8b362cce1acf9425a9fe
Haut-Stone <haut.1604.stone@gmail.com>
2017-01-23 14:24:50 +0800

    update

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@
 * @Author: Haut-Stone
 * @Date:   2017-01-22 11:12:17
 * @Last Modified by:   Haut-Stone
-* @Last Modified time: 2017-01-22 21:58:30
+* @Last Modified time: 2017-01-23 13:52:08
 */
 #include <iostream>
 #include <cstdio>
@@ -15,26 +15,13 @@
 #include <cmath>
 #include <cstring>
 using namespace std;
-const int N = 110;
+
+
 
 int main(void)
 {
-	int m,n;
-	int dp[N][N];
-	int raw[N][N];
-	//m row n col
-	while(scanf("%d%d", &m, &n) != EOF){
-		for(int i=1;i<=m;i++){
-			for(int j=1;j<=n;j++){
-				scanf("%d", &raw[i][j]);
-			}
-		}
-		for(int i=1;i<=m;i++){
-			for(int j=1;j<=n;j++){
-				dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + raw[i][j];
-			}
-		}
-		printf("%d\n", dp[m][n]);
-	}
+	char string[110];
+	scanf("%s", string);
+	
 	return 0;
 }
