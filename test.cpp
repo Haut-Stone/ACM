31c266432f00b0be665f7d0e8fc115eb80a09f87
Haut-Stone <haut.1604.stone@gmail.com>
2017-01-25 20:03:51 +0800

    update

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@
 * @Author: Haut-Stone
 * @Date:   2017-01-22 11:12:17
 * @Last Modified by:   Haut-Stone
-* @Last Modified time: 2017-01-23 19:12:43
+* @Last Modified time: 2017-01-25 20:02:09
 */
 #include <iostream>
 #include <cstdio>
@@ -16,12 +16,29 @@
 #include <cstring>
 using namespace std;
 
-
-const int N = 10e6;
-
+const int N = 110;
 
 int main()
 {
-	
+	int n;
+	char str[N];
+	int index[N];
+	scanf("%d", &n);
+	getchar();
+	while(n--){
+		gets(str);
+		for(int i=1;i<=strlen(str);i++){
+			scanf("%d", &index[i]);
+		}
+		getchar();
+		for(int i=1;i<=strlen(str);i++){
+			for(int j=1;j<=strlen(str);j++){
+				if(i == index[j]){
+					printf("%c", str[j-1]);
+				}
+			}
+		}
+		printf("\n");
+	}
     return 0;
 }
