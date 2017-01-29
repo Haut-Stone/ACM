325cc6a72ca633041b1b19802afbf425d1a7e0cd
Haut-Stone <haut.1604.stone@gmail.com>
2017-01-29 23:16:31 +0800

    DFS经典题目，DFS还是要多写才有感觉,其实还是蛮难的

---
+++ DFS_棋盘问题.cpp
@@ -0,0 +1,67 @@
+/*
+* Created by ShiJiahuan(li) in haut.
+* for more please visit www.shallweitalk.com
+* 
+* Copyright 2017 SJH. All rights reserved.
+*
+* @Author: Haut-Stone
+* @Date:   2017-01-29 17:10:06
+* @Last Modified by:   Haut-Stone
+* @Last Modified time: 2017-01-29 23:15:47
+*/
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <map>
+#include <vector>
+#include <ctime> 
+using namespace std;
+
+const int N = 10;
+char myMap[N][N];
+int cluUsed[100];
+int ans;
+int n;
+int k;
+//读错题意了，只有#的地方可以放棋子。
+//
+void dfs(int row, int k)//逐行搜索。。
+{
+	if(k==0){//符合条件的填满所有棋子
+		ans++;
+		return;
+	}
+
+	for(int i=row;i<n;i++){
+		for(int j=0;j<n;j++){
+			if(myMap[i][j] == '.' || cluUsed[j] == 1){
+				continue;
+			}
+			cluUsed[j] = 1;//标记该列是否已经被占用。
+			dfs(i+1, k-1);
+			cluUsed[j] = 0;
+		}
+	}
+}
+
+int main(void)
+{
+
+	while(scanf("%d%d", &n, &k), n!=-1 && k!=-1){
+		getchar();
+		ans = 0;
+		memset(myMap, '\0', sizeof(myMap));
+        memset(cluUsed, 0, sizeof(cluUsed));
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+				scanf("%c", &myMap[i][j]);//载入地图，没有留边界空余部分
+			}
+			getchar();
+		}
+		dfs(0, k);//从0开始搜索
+		printf("%d\n", ans);
+	}
+    return 0;
+}
