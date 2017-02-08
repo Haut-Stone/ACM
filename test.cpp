dca029ee08efec43d25e8a94d0c2f3128ed32fcc
Haut-Stone <haut.1604.stone@gmail.com>
2017-02-08 23:40:19 +0800

    新生赛的一道题，现在总算是看明白了，但是这道题还可以用dfs做！

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@
 * @Author: Haut-Stone
 * @Date:   2017-01-22 11:12:17
 * @Last Modified by:   Haut-Stone
-* @Last Modified time: 2017-02-08 11:19:50
+* @Last Modified time: 2017-02-08 17:32:40
 */
 #include <iostream>
 #include <cstdio>
@@ -17,37 +17,11 @@
 #include <string>
 using namespace std;
 
+#define N 1000
 int main()
 {
-    int digit1, digit2;
-    int a[100],b[100];
-    int i, j, k, sum = 0;
-    scanf("%d %d", &digit1, &digit2);
-    for(i = 0; i < digit1; i++){
-        scanf("%d", &a[i]);
+    for(int i=0;i<N;i++){
+    	printf("%d\\", i);
     }
-    for(j = 0; j < digit2; j++){
-        scanf("%d", &b[j]);
-    }
-    for(i = 0;i < digit1;i++){
-        if(b[0] == a[i]){
-            k = i;
-            i++;
-            for(j = 1;j < digit2;j++,i++){
-                if(b[j]==a[i]){
-                    sum++;
-                }   
-                else{
-                    break;
-                }
-            }
-        }
-        if(j == digit2)
-        break;
-    }
-    if(j == digit2 && sum != 0)
-        printf("%d\n",k);
-    else
-    printf("No Answer\n");
     return 0;
 }
--- 轻院新生赛--以为是贪心。。.cpp
+++ 轻院新生赛--以为是贪心。。.cpp
@@ -7,8 +7,10 @@
 * @Author: Haut-Stone
 * @Date:   2016-12-26 12:52:30
 * @Last Modified by:   Haut-Stone
-* @Last Modified time: 2016-12-26 12:52:30
+* @Last Modified time: 2017-02-08 23:15:12
 */
+//这原来是一个背包啊。。。！！！
+//厉害了，这道题里面有模版....
 #include <iostream>
 #include <cstdio>
 #include <algorithm>
@@ -16,9 +18,52 @@
 #include <cstring>
 using namespace std;
 
+struct Node
+{
+	int left;
+	int right;
+	int value;
+};
+
+
+bool cmp(Node x, Node y)
+{
+	if(x.right != y.right){
+		return x.right < y.right;
+	}else{
+		return x.left < y.left;
+	}
+}
 
 int main(void)
 {
-
+	int T;
+	int n;
+	int ans;
+	int temp;
+	Node solo[100];
+	scanf("%d", &T);
+	while(T--){
+		int dp[100];
+		memset(dp, 0, sizeof(dp));
+		temp = 0;
+		scanf("%d", &n);
+		for(int i=0;i<n;i++){
+			scanf("%d %d %d", &solo[i].left, &solo[i].right, &solo[i].value);
+		}
+		sort(solo, solo+n, cmp);
+		for(int i=0;i<n;i++){
+			ans = 0;
+			dp[i] = solo[i].value;
+			for(int j=0;j<i;j++){
+				if(solo[i].left > solo[j].right){
+					temp = max(temp, dp[j]);
+				}
+			}
+			dp[i] += temp;
+			ans = max(ans, dp[i]);
+		}
+		printf("%d\n", ans);
+	}
     return 0;
 }
