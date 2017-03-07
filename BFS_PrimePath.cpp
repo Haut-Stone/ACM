7219b0f2dc5a1be15748412cc86f5311bc0d10fb
Haut-Stone <haut.1604.stone@gmail.com>
2017-03-07 22:10:37 +0800

    BFS又一波

--- BFS_PrimePath.cpp
+++ BFS_PrimePath.cpp
@@ -7,7 +7,7 @@
 * @Author: Haut-Stone
 * @Date:   2017-03-02 15:24:33
 * @Last Modified by:   Haut-Stone
-* @Last Modified time: 2017-03-02 19:43:29
+* @Last Modified time: 2017-03-07 22:10:23
 */
 
 //http://vj.sdutacm.org/contest/view.action?cid=62898#problem/B
@@ -29,6 +29,7 @@ int prime[maxN];
 
 int initPrime(int n)
 {
+	//用于判定1000-maxN范围内的素数
 	int k = sqrt(n);
 	for(int i=2;i<=k;i++){
 		if(n%i == 0) return 0;
@@ -50,24 +51,24 @@ int bfs(int a, int b)
 	int vis[maxN] = {0};//踪迹数组
 	int q;
 	queue<int>Q;
-	Q.push(a);
-	vis[a] = 1;
+	Q.push(a);//将原始数加入队列
+	vis[a] = 1;//标记这个数已经被用过
 
-	while(Q.size()){
+	while(Q.size()){//队列非空的情况下
 		a = Q.front();
 		Q.pop();
-		if(a == b) return vis[a]-1;
+		if(a == b) return vis[a]-1;//如果？？
 		int t = 1000;
 		for(int i=0;i<4;i++){
 			q = turn(a, i);//改变各位的数字为0
 			for(int k=0;k<10;k++){
-				int j = q+k*t;
+				int j = q+k*t;//用q加上1000->9000
 				if(prime[j] == 1 && vis[j] == 0){
 					Q.push(j);
-					vis[j] = vis[a] + 1;
+					vis[j] = vis[a] + 1;//步数增加
 				}
 			}
-			t /= 10;
+			t /= 10;//t的位阶改变。
 		}
 	}	
 	return -1;
@@ -79,13 +80,13 @@ int main(void)
 	int ans;
 
 	for(int i=1000;i<maxN;i++){
-		prime[i] = initPrime(i);
+		prime[i] = initPrime(i);//首先先把素数全部找出来
 	}
 
 	scanf("%d", &n);
 	while(n--){
-		scanf("%d%d", &a, &b);
-		ans = bfs(a, b);
+		scanf("%d%d", &a, &b);//原始数，和目标数
+		ans = bfs(a, b);//bfs过程返回步数
 		if(ans == -1){
 			printf("Impossible\n");
 		}else{
