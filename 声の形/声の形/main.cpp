b876e83152f35f6396f7eb2d4da6a4f327f5eb77
Haut-Stone <haut.1604.stone@gmail.com>
2017-09-27 10:46:33 +0800

    update

---
+++ 声の形/声の形/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  声の形
+//
+//  Created by Jack Lee on 27/09/2017.
+//  Copyright © 2017 SJH. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
